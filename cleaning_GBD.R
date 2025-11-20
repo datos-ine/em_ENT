4cdd3eb86d95a3d2776c7c4e560cbcd279f4c907
datos-ine <ine.datos@gmail.com>
2025-11-20 12:43:33 -0300

    Update 20-11-2025

--- cleaning_GBD.R
+++ cleaning_GBD.R
@@ -1,7 +1,7 @@
 ### Exceso de Mortalidad por ENT en Argentina durante la pandemia de COVID-19
 ### Limpieza códigos ICD-10 para ENT y garbage code según categorías GBD (2023)
 ### Autora: Tamara Ricardo
-# Última modificación: 20-11-2025 09:12
+# Última modificación: 20-11-2025 12:34
 
 # Cargar paquetes --------------------------------------------------------
 pacman::p_load(
@@ -12,10 +12,15 @@ pacman::p_load(
 
 
 # Cargar datos -----------------------------------------------------------
+## Códigos CIE10 - GBD 2023
 icd10_raw <- import(
   "raw/IHME_GBD_2021_COD_CAUSE_ICD_CODE_MAP_Y2024M05D16_0.XLSX",
   skip = 1
 )
+
+## Códigos CIE10 - DEIS
+deis <- import("clean/deis_defun_mensuales_2010_2022.csv") |>
+  select(starts_with("cie"))
 
 
 # Limpiar dataset --------------------------------------------------------
@@ -44,8 +49,61 @@ icd10 <- icd10_raw |>
   # Separar en filas
   separate_rows(icd10, sep = ", ") |>
 
-  # Corregir código mal cargado
-  mutate(icd10 = if_else(icd10 == "I99-ID5.9", "I99.0", icd10)) |>
+  # Filtrar códigos repetidos
+  filter(
+    !icd10 %in%
+      c(
+        "C83",
+        "C68",
+        "C69",
+        "D01",
+        "D07",
+        "D13",
+        "D36.0",
+        "D44",
+        "D48",
+        "I27",
+        "I50",
+        "I51",
+        "I67",
+        "J81",
+        "K92",
+        "N84",
+        "N95",
+        "R07",
+        "R58"
+      )
+  ) |>
+
+  # Corregir códigos mal cargados
+  mutate(
+    icd10 = case_when(
+      icd10 == "A47-A48" ~ "A47.0-A47.9",
+      icd10 == "A48.8-A49" ~ "A48.8-A48.9",
+      icd10 == "B92-B94" ~ "B92.0-B93.9",
+      icd10 == "D01.4-D02" ~ "D01.4-D01.9",
+      icd10 == "D07.6-D09" ~ "D07.6-D08.9",
+      icd10 == "D09.9-D10" ~ "D09.9",
+      icd10 == "D13.9-D14" ~ "D13.9",
+      icd10 == "D28.9-D29" ~ "D28.9",
+      icd10 == "D29.9-D30" ~ "D29.9",
+      icd10 == "D31-D36" ~ "D31.0-D35.9",
+      icd10 == "D37.6-D38" ~ "D37.6-D37.9",
+      icd10 == "D39.9-D40" ~ "D39.9",
+      icd10 == "D40.9-D41" ~ "D40.9",
+      icd10 == "D63.8-D64" ~ "D63.8-D64.0",
+      icd10 == "G91.4-G93" ~ "G91.4-G92.9",
+      icd10 == "G99-H05" ~ "G99.0-H05.1",
+      icd10 == "I68.8-I69" ~ "I68.8",
+      icd10 == "I99-ID5.9" ~ "I99.0",
+      icd10 == "M65.1-M71" ~ "M65.1-M71.1",
+      icd10 == "R03.1-R04" ~ "R03.1-R03.9",
+      icd10 == "R19.8-R23" ~ "R19.8-R22.9",
+      icd10 == "R74-R78" ~ "R74.0-R77.9",
+      icd10 == "Y86-Y87" ~ "Y86.0-Y86.9",
+      .default = icd10
+    )
+  ) |>
 
   # Separar inicio y fin secuencia
   separate_wider_delim(
@@ -93,10 +151,10 @@ icd10 <- icd10_raw |>
   # Añadir filas para cuando cambia la letra (G99-H05)
   add_row(
     cause = rep("Garbage Code (GBD Level 1)", 2),
-    icd10 = rep("G99-H05", 2),
+    icd10 = rep("G99.0-H05.1", 2),
     name = c("end", "start"),
     letra = c("G", "H"),
-    numero = c(99.9, 0.0)
+    numero = c(99.9, 0.1)
   ) |>
 
   # Añadir filas para cuando cambia la letra (S00-T98.3)
@@ -142,6 +200,53 @@ icd10 <- icd10_raw |>
 
   # Regenerar códigos
   mutate(cie10_cod = paste0(letra, sprintf("%04.1f", numero)))
+
+
+# Probar merge con dataset defunciones -----------------------------------
+left_join(deis, icd10) |> warnings()
+
+
+## Códigos repetidos/mal cargados
+deis[40, "cie10_cod"] # J81.0: edema pulmonar
+deis[83, "cie10_cod"] # G93.2: hipertensión intracraneal benigna
+deis[129, "cie10_cod"] # I50.0: falla cardíaca congestiva
+deis[404, "cie10_cod"] # K92.0: Haematemesis
+deis[676, "cie10_cod"] # I69.0: Secuelas de hemorragia subaracnoidea
+deis[726, "cie10_cod"] # D64.9: Anemia no especificada
+deis[1008, "cie10_cod"] # R04.2: Haemoptysis
+deis[1052, "cie10_cod"] # D38.3: Mediastinum
+deis[1406, "cie10_cod"] # I27.0: Hipertensión pulmonar primaria
+deis[1944, "cie10_cod"] # D41.0: Neoplasia de riñón
+deis[4082, "cie10_cod"] # R58.0: Hemorragia no clasificada
+deis[4262, "cie10_cod"] # A48.0: Gas gangrene
+deis[5103, "cie10_cod"] # Y87.2: Secuelas de daño autoinfligido
+deis[7733, "cie10_cod"] # D40.1: Neoplasia de testículo
+deis[8007, "cie10_cod"] # A49.8: Otras infecciones bacterianas
+deis[9079, "cie10_cod"] # I51.0: Cardiac septal defect, acquired
+deis[9326, "cie10_cod"] # R07.0: Dolor de garganta
+deis[9745, "cie10_cod"] # D48.0: Neoplasia de hueso y cartílago
+deis[9894, "cie10_cod"] # D29.1: Neoplasia de próstata
+deis[18798, "cie10_cod"] # C83.0: Small cell B-cell lymphoma
+deis[19592, "cie10_cod"] # I67.0: Dissection of cerebral arteries, nonruptured
+deis[20320, "cie10_cod"] # D44.0: Neoplasia de tiroides
+deis[20320, "cie10_cod"] # D44.0: Neoplasia de tiroides
+deis[32407, "cie10_cod"] # D36.7: Neoplasia de otros sitios específicos
+deis[34984, "cie10_cod"] # C68.0: Cáncer de uretra
+deis[65296, "cie10_cod"] # C69.0: Cáncer de conjuntiva
+deis[67900, "cie10_cod"] # D30.0: Neoplasia de riñón
+deis[113117, "cie10_cod"] # D09.9: Carcinoma in situ no especificado
+deis[267390, "cie10_cod"] # D02.2: Carcinoma de bronquio y pulmón
+deis[354010, "cie10_cod"] # B94.8: Secuelas de enf. infecciosas o parasitarias
+deis[362016, "cie10_cod"] # N84.0: Polyp of corpus uteri
+deis[373712, "cie10_cod"] # M71.2: Synovial cyst of popliteal space
+deis[440539, "cie10_cod"] # R23.0: Cyanosis
+deis[482906, "cie10_cod"] # N95.0: Sangrado postmenopausia
+deis[502762, "cie10_cod"] # D01.0: Carcinoma de colon
+deis[878972, "cie10_cod"] # D13.0: Neoplasia de esófago
+deis[1114959, "cie10_cod"] # D07.0: Carcinoma de endometrio
+deis[1176066, "cie10_cod"] # D10.3: Neoplasma de boca no especificado
+deis[1176576, "cie10_cod"] # H05.5: Retrobulbar foreign body
+deis[1244896, "cie10_cod"] # R78.0: Alcohol en sangre
 
 
 # Exportar datos limpios -------------------------------------------------
--- recoding_causas_ENT.R
+++ recoding_causas_ENT.R
@@ -1,7 +1,8 @@
 ### Exceso de mortalidad por ENTs en Argentina durante la pandemia de COVID-19
-### Categorización de causas de muerte y reasignación de códigos basura cie-10
+### Categorización de causas de muerte y reasignación de códigos basura CIE-10
+### según tabla GBD 2023 y metodología propuesta por Teixeira et al. (2023)
 ### Autora: Tamara Ricardo
-# Última modificación: 13-11-2025 09:09
+# Última modificación: 20-11-2025 12:43
 
 # Cargar paquetes --------------------------------------------------------
 pacman::p_load(
@@ -12,318 +13,42 @@ pacman::p_load(
 
 
 # Cargar datos -----------------------------------------------------------
-## Códigos CIE-10 2021
-cie10 <- import(
-  "raw/IHME_GBD_2021_COD_CAUSE_ICD_CODE_MAP_Y2024M05D16.XLSX",
-  skip = 1
-)
+## Códigos CIE-10 2021 (GBD)
+codigos_cie10 <- import("clean/gbd_2023_codes.csv")
+
+## Proyecciones poblacionales mensuales (INDEC)
+proy_mensual <- import("clean/indec_pob_mensual_2010_2022.csv")
 
 ## Mortalidad mensual en Argentina
-datos <- import("clean/defun_mensuales_arg_2010_2022.csv")
+defun <- import("clean/deis_defun_mensuales_2010_2022.csv")
 
 
 # Explorar mortalidad por grupo de causas --------------------------------
-datos |>
-  tabyl(cie10_cat) |>
+defun |>
+  tabyl(cie10_grupo) |>
   adorn_pct_formatting()
 
 
 # Recategorizar causas de muerte -----------------------------------------
-datos_causa <- datos |>
-
-  # Recategorizar grupos CIE-10
-  mutate(
-    cie10_grupo = case_when(
-      str_detect(cie10_cat, "0200|0299") ~ "Neoplasias",
-      str_detect(cie10_cat, "0300") ~ "Diabetes mellitus",
-      str_detect(cie10_cat, "0900") ~ "Enfermedades cardiovasculares",
-      between(cie10_cod, "J30.0", "J98.9") ~
-        "Enfermedades respiratorias crónicas",
-      str_detect(cie10_cat, "1700") ~ "Causas externas",
-    ),
-    .after = cie10_cat
+datos <- defun |>
+  # Añadir códigos CIE10 - GBD 2023
+  left_join(
+    codigos_cie10 |>
+      select(-letra, -numero)
   ) |>
 
-  #
-
-  # Reasignar causas de muerte
+  # Cambiar etiquetas categorías
   mutate(
-    causa_cat = case_when(
-      ### COVID-19 ###
-      between(cie10_cod, "U07.1", "U07.2") ~ "COVID-19",
-
-      ### Enfermedad de Parkinson ###
-      cie10_cod == "G20.X" ~ "Enfermedad de Parkinson",
-
-      ### Suicidio ###
-      between(cie10_cod, "X60.0", "X84.9") ~ "Suicidio",
-
-      ### Alzheimer y otras demencias ###
-      between(cie10_cod, "F00.0", "F02.0") |
-        between(cie10_cod, "F02.8", "F03.9") |
-        between(cie10_cod, "G30.0", "G31.1") |
-        between(cie10_cod, "G31.8", "G31.9") ~
-        "Alzheimer y otras demencias",
-
-      ### Trastornos por uso de alcohol ###
-      between(cie10_cod, "F10.0", "F10.9") |
-        between(cie10_cod, "K70.0", "K70.9") |
-        between(cie10_cod, "K74.0", "K74.9") |
-        between(cie10_cod, "K76.0", "K76.2") |
-        between(cie10_cod, "K76.4", "K76.9") |
-        between(cie10_cod, "X45.0", "X45.9") |
-        between(cie10_cod, "Y15.0", "Y15.9") |
-        cie10_cod == "G31.2" ~
-        "Trastornos por uso de alcohol",
-
-      ### Trastornos por uso de drogas ###
-      between(cie10_cod, "F11.0", "F16.9") |
-        between(cie10_cod, "F18.0", "F19.9") |
-        between(cie10_cod, "X40.0", "X44.9") |
-        between(cie10_cod, "Y10.0", "Y14.9") ~
-        "Trastornos por uso de drogas",
-
-      ### Diabetes Mellitus ###
-      between(cie10_cod, "E10.0", "E11.9") &
-        !cie10_cod %in% c("E10.2", "E11.2") ~ # Excluir complicaciones renales
-        "Diabetes Mellitus",
-
-      ### Enfermedades respiratorias crónicas ###
-      between(cie10_cod, "J30.0", "J35.9") |
-        between(cie10_cod, "J37.0", "J37.9") |
-        between(cie10_cod, "J40.0", "J46.9") |
-        between(cie10_cod, "J60.0", "J63.8") |
-        between(cie10_cod, "J65.0", "J68.9") |
-        between(cie10_cod, "J70.8", "J70.9") |
-        between(cie10_cod, "J84.0", "J84.9") |
-        between(cie10_cod, "J91.0", "J92.9") |
-        between(cie10_cod, "D86.0", "D86.2") |
-        cie10_cod %in% c("D86.9", "G47.3", "J70.0", "J82.0") ~
-        "Enfermedades respiratorias crónicas",
-
-      ### Enfermedades cardiovasculares ###
-      between(cie10_cod, "I01.0", "I02.0") |
-        between(cie10_cod, "I05.0", "I09.9") | # Enfermedades reumáticas del corazón
-        between(cie10_cod, "I11.0", "I11.9") | # Enfermedad hipertensiva del corazón
-        between(cie10_cod, "I20.0", "I25.9") | # Enfermedad isquémica del corazón
-        between(cie10_cod, "I28.0", "I28.8") |
-        between(cie10_cod, "I30.0", "I31.1") | # Pericarditis
-        between(cie10_cod, "I31.8", "I37.8") |
-        between(cie10_cod, "I38.0", "I41.9") |
-        between(cie10_cod, "I42.1", "I42.8") |
-        between(cie10_cod, "I43.0", "I43.9") |
-        between(cie10_cod, "I47.0", "I48.9") |
-        between(cie10_cod, "I51.0", "I51.4") |
-        between(cie10_cod, "I60.0", "I63.9") |
-        between(cie10_cod, "I65.0", "I67.3") |
-        between(cie10_cod, "I67.5", "I67.6") |
-        between(cie10_cod, "I68.0", "I68.2") |
-        between(cie10_cod, "I69.0", "I69.3") |
-        between(cie10_cod, "I70.2", "I70.8") |
-        between(cie10_cod, "I71.0", "I73.9") |
-        between(cie10_cod, "I77.0", "I83.9") |
-        between(cie10_cod, "I86.0", "I89.0") |
-        between(cie10_cod, "G45.0", "G46.8") |
-        cie10_cod %in% c("B33.2", "I89.9", "I98.0", "K75.1") ~
-        "Enfermedades cardiovasculares",
-
-      ### Neoplasias ###
-      between(cie10_cod, "C00.0", "C13.9") |
-        between(cie10_cod, "C15.0", "C25.9") |
-        between(cie10_cod, "C30.0", "C34.9") |
-        between(cie10_cod, "C37.0", "C38.8") |
-        between(cie10_cod, "C40.0", "C41.9") |
-        between(cie10_cod, "C43.0", "C45.9") |
-        between(cie10_cod, "C47.0", "C54.9") |
-        between(cie10_cod, "C56.0", "C57.8") |
-        cie10_cod == "C58" |
-        between(cie10_cod, "C60.0", "C63.8") |
-        between(cie10_cod, "C64.0", "C67.9") |
-        between(cie10_cod, "C68.0", "C68.8") |
-        between(cie10_cod, "C69.0", "C75.8") |
-        between(cie10_cod, "C81.0", "C86.6") |
-        between(cie10_cod, "C88.0", "C96.9") |
-        between(cie10_cod, "D00.1", "D00.2") |
-        between(cie10_cod, "D01.0", "D01.3") |
-        between(cie10_cod, "D02.0", "D02.3") |
-        between(cie10_cod, "D03.0", "D06.9") |
-        between(cie10_cod, "D07.0", "D07.2") |
-        between(cie10_cod, "D07.4", "D07.5") |
-        between(cie10_cod, "D09.2", "D09.3") |
-        between(cie10_cod, "D10.0", "D10.7") |
-        between(cie10_cod, "D11.0", "D12.9") |
-        between(cie10_cod, "D13.0", "D13.7") |
-        between(cie10_cod, "D14.0", "D14.3") |
-        between(cie10_cod, "D15.0", "D16.9") |
-        between(cie10_cod, "D22.0", "D24.9") |
-        between(cie10_cod, "D26.0", "D27.9") |
-        between(cie10_cod, "D28.0", "D28.1") |
-        between(cie10_cod, "D29.0", "D29.8") |
-        between(cie10_cod, "D30.0", "D30.8") |
-        between(cie10_cod, "D31.0", "D36.0") |
-        between(cie10_cod, "D36.1", "D36.7") |
-        between(cie10_cod, "D37.1", "D37.5") |
-        between(cie10_cod, "D38.0", "D38.5") |
-        between(cie10_cod, "D39.1", "D39.2") |
-        between(cie10_cod, "D40.0", "D40.8") |
-        between(cie10_cod, "D41.0", "D41.8") |
-        between(cie10_cod, "D42.0", "D43.9") |
-        between(cie10_cod, "D44.0", "D44.8") |
-        between(cie10_cod, "D45.0", "D47.9") |
-        between(cie10_cod, "D48.0", "D48.6") |
-        between(cie10_cod, "D49.2", "D49.4") |
-        cie10_cod %in% c("D09.0", "D09.8", "D28.7", "D39.8", "D49.6") |
-        between(cie10_cod, "K62.0", "K62.1") |
-        cie10_cod == "K63.5" |
-        between(cie10_cod, "N60.0", "N60.9") |
-        between(cie10_cod, "N84.0", "N84.1") |
-        between(cie10_cod, "N87.0", "N87.9") ~
-        "Neoplasias",
-    )
-  ) |>
-
-  ## Identificar códigos garbage nivel 3 y 4
-  mutate(
-    gc_ent = case_when(
-      ### Diabetes mellitus ###
-      between(cie10_cod, "E12.0", "E12.1") |
-        between(cie10_cod, "E12.3", "E13.1") |
-        between(cie10_cod, "E13.3", "E14.1") |
-        between(cie10_cod, "E14.3", "E14.9") |
-        cie10_cod %in% c("R73", "R73.0", "R73.9") ~
-        "GC diabetes mellitus",
-
-      ### Enfermedades respiratorias crónicas ###
-      between(cie10_cod, "J40.0", "J40.9") |
-        between(cie10_cod, "J47.0", "J59.9") |
-        between(cie10_cod, "J71.0", "J79.9") |
-        between(cie10_cod, "J96.1", "J96.8") |
-        between(cie10_cod, "J97.0", "J98.0") |
-        between(cie10_cod, "J98.4", "J99.1") |
-        cie10_cod %in%
-          c(
-            "J83.0",
-            "J85.9",
-            "J87.0",
-            "J88.0",
-            "J89.0",
-            "J90.9",
-            "J93.6",
-            "J99.8"
-          ) ~
-        "GC respiratorias crónicas",
-
-      ### Enfermedades cardiovasculares ###
-      between(cie10_cod, "I03.0", "I04.9") | # No existen
-        between(cie10_cod, "I14.0", "I19.9") | # I16 a I19 no existen
-        between(cie10_cod, "I27.0", "I29.9") | # I29 no existe
-        between(cie10_cod, "I27.0", "I29.9") | # I29 no existe
-        between(cie10_cod, "I42.0", "I42.9") |
-        between(cie10_cod, "I44.0", "I45.9") |
-        between(cie10_cod, "I49.0", "I49.9") |
-        between(cie10_cod, "I51.0", "I51.6") |
-        between(cie10_cod, "I51.8", "I59.9") | # I53-I59 no existen
-        between(cie10_cod, "I67.8", "I69.9") |
-        str_detect(cie10_cod, "I00|I10|I64") |
-        cie10_cod %in% c("I37.9") ~
-        "GC enfermedades cardiovasculares",
+    cie10_gbd_cat = case_when(
+      str_detect(cause, "Alcohol") ~ "Alcoholismo",
+      str_detect(cause, "Alzheimer") ~ "Alzheimer/demencias",
+      str_detect(cause, "Cardiovascular") ~ "Cardiovascular",
+      str_detect(cause, "Chronic") ~ "Respiratoria crónica",
+      str_detect(cause, "Drug") ~ "Uso de drogas",
+      str_detect(cause, "Neoplasms") ~ "Neoplasias",
+      str_detect(cause, "Parkinson") ~ "Parkinson",
+      str_detect(cause, "Self-harm") ~ "Suicidio",
+      is.na(cause) ~ "Otras causas",
+      .default = cause
     )
   )
-
-#   I64 - I64.9,
-#   I67,
-
-#   I68.8 - I69,
-#   I69.4 -
-#     I70.1,
-#   I70.9,
-#   I74 - I75.8,
-#   I90 - I94,
-#   I96 - I96.9,
-#   I98.4,
-#   I98.8,
-#   I99,
-#   I99.0,
-#   I99.8,
-#   I99.9,
-#   ID59
-# )
-# Neoplasias(
-#   C14 - C14.9,
-#   C22.9,
-#   C26 - C36,
-#   C39 - C39.9,
-#   C42,
-#   C55 - C55.9,
-#   C57.9,
-#   C59,
-#   C6,
-#   C63.9,
-#   C68,
-#   C68.9 - C99.9,
-#   C74 -
-#     C74.9,
-#   C75.9 - C79.9,
-#   C8 - C80.9,
-#   C87,
-#   C97 - C99,
-#   D0 - D00.0,
-#   D01,
-#   D01.4 - D02.9,
-#   D07,
-#   D07.3,
-#   D07.6 - D09,
-#   D09.1,
-#   D09.7,
-#   D09.9 - D10.9,
-#   D13,
-#   D13.9,
-#   D14,
-#   D14.4,
-#   D17 - D21.9,
-#   D28,
-#   D28.9,
-#   D29,
-#   D29.9,
-#   D30,
-#   D30.9,
-#   D36.0,
-#   D36.9 - D37.0,
-#   D37.6 - D38,
-#   D38.6,
-#   -D39.0,
-#   D39.7,
-#   D39.9,
-#   D4 - D40,
-#   D40.9 -
-#     D41.9,
-#   D44,
-#   D44.9,
-#   DD48,
-#   D48.7 - D49.5,
-#   D49.7 - D49.9,
-#   D54,
-#   N84.2 - N84.3,
-#   N84.8
-# )
-
-# Doenças
-# respiratórias
-# crônicas(
-#   J40,
-#   J40.0,
-#   J40.9,
-#   J47 - J59,
-#   J71 - J79,
-#   J83,
-#   J85.9,
-#   J87,
-#   J88,
-#   J89,
-#   J90.9,
-#   J93.6,
-#   J96.1 - J96.8,
-#   J97 - J98.0,
-#   J98.4 - J99.1,
-#   J99.8
-# )
